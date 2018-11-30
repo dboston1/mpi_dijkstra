a911b7384f8afbd59181f73143a0362c7191d22b
dboston1 <dboston1@rams.colostate.edu>
2018-11-29 23:30:20 -0700

    test4

--- src_mpi/map.cpp
+++ src_mpi/map.cpp
@@ -61,7 +61,7 @@ Map Map::fromFile(std::ifstream&& istream, const char delimiter) {
             //this won't work if verticesCount gets too large; may need another naming convention
             ss << i;
             std::string nodeName = ss.str();
-            ss.clear();
+            ss.str(std::string());
             std::cout << "Adding node: " << nodeName << std::endl;
             m.nodesNames.push_back(std::string(nodeName));
         }
