c61ddc0c408beb703c6f5e5526d85c266d469202
dboston1 <dboston1@rams.colostate.edu>
2018-11-30 19:59:27 -0700

    testing 3

--- src_mpi/dijkstra.cpp
+++ src_mpi/dijkstra.cpp
@@ -318,7 +318,6 @@ void dijkstraWorker(int mpiNodeId, int mpiNodesCount) {
                 int row_index = (int)((node-1) / dim);
                 int col_index = (node-1) % dim;
                 auto nodeDistance = 0;
-                std::cout << "weights[0][0]: " << weights[0][0] << std::endl;
                 if(node != (nodesCount -1)){
                     nodeDistance = weights[row_index][col_index];
                 }
